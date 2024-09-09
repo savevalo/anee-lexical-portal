a396a021e36538198b8a8ce575d237d04d7f9bdf
Sam Hardwick <sam.hardwick@csc.fi>
2024-09-09 15:16:18 +0300

    Make (more) idempotent

--- link-portal-package.sh
+++ link-portal-package.sh
@@ -3,19 +3,21 @@ set -e
 set -x
 
 # Configuration
-GEXF_BASE_DIR=$(pwd)
+GEXF_BASE_DIR=$(realpath $(pwd))
 
 PACKAGEPATH=$(realpath $1)
 ORIG_DIR=$(pwd)
 
-ln -s $GEXF_BASE_DIR/config.js $PACKAGEPATH
-ln -s $GEXF_BASE_DIR/img $PACKAGEPATH
-ln -s $GEXF_BASE_DIR/index.html $PACKAGEPATH
-ln -s $GEXF_BASE_DIR/js $PACKAGEPATH
-ln -s $GEXF_BASE_DIR/styles $PACKAGEPATH
+ln -sf $GEXF_BASE_DIR/config.js $PACKAGEPATH
+ln -sf $GEXF_BASE_DIR/img $PACKAGEPATH
+ln -sf $GEXF_BASE_DIR/index.html $PACKAGEPATH
+ln -sf $GEXF_BASE_DIR/js $PACKAGEPATH
+ln -sf $GEXF_BASE_DIR/styles $PACKAGEPATH
 
 cd $PACKAGEPATH
-ln -s $(find . -maxdepth 1 -name "*.gexf") index.gexf
-ln -s $(find . -maxdepth 1 -name "*.json") index.json
+rm index.gexf
+rm index.json
+ln -sf $(find . -maxdepth 1 -name "*.gexf") index.gexf
+ln -sf $(find . -maxdepth 1 -name "*.json") index.json
 
 cd $ORIG_DIR

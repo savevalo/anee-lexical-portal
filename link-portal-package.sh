f9205b4530e7f23527a2adf1fd58029fa6f4fad0
Sam Hardwick <sam.hardwick@csc.fi>
2024-09-09 22:18:01 +0300

    Relative targets

--- link-portal-package.sh
+++ link-portal-package.sh
@@ -8,13 +8,13 @@ GEXF_BASE_DIR=$(realpath $(pwd))
 PACKAGEPATH=$(realpath $1)
 ORIG_DIR=$(pwd)
 
-ln -sf $GEXF_BASE_DIR/config.js $PACKAGEPATH/config.js
-ln -sf $GEXF_BASE_DIR/img $PACKAGEPATH/img
-ln -sf $GEXF_BASE_DIR/index.html $PACKAGEPATH/index.html
-ln -sf $GEXF_BASE_DIR/js $PACKAGEPATH/js
-ln -sf $GEXF_BASE_DIR/styles $PACKAGEPATH/styles
-
 cd $PACKAGEPATH
+ln -sf $GEXF_BASE_DIR/config.js ./
+ln -sf $GEXF_BASE_DIR/img ./
+ln -sf $GEXF_BASE_DIR/index.html ./
+ln -sf $GEXF_BASE_DIR/js ./
+ln -sf $GEXF_BASE_DIR/styles ./
+
 rm -f index.gexf
 rm -f index.json
 ln -sf $(find . -maxdepth 1 -name "*.gexf") index.gexf

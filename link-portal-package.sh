7eabfadc7bdc9de8cd73fb102171bcdbd8347a6f
Sam Hardwick <sam.hardwick@csc.fi>
2025-04-25 10:47:02 +0300

    Rewrite links pointing to old domain

--- link-portal-package.sh
+++ link-portal-package.sh
@@ -23,5 +23,6 @@ rm -f index.gexf
 rm -f index.json
 ln -sf $(find . -maxdepth 1 -name "*.gexf") index.gexf
 ln -sf $(find . -maxdepth 1 -name "*.json") index.json
+sed --in-place 's/https:\/\/korp\.csc\.fi\/korp-test\/oracc2021/https:\/\/kielipankki.fi\/korp/g' index.gexf index.json
 
 cd $ORIG_DIR
